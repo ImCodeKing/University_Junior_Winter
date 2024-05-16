f04f82f253609b5d9acf9c031dffc08f8cd2a43f
ImCodeKing <lizhaoya21@mails.tsinghua.edu.cn>
2024-05-16 20:29:32 +0800

    init

--- 人工智能原理/编程2/morpho_minist.py
+++ 人工智能原理/编程2/morpho_minist.py
@@ -22,7 +22,7 @@ test_size = test_label['size']
 print(f"训练集的尺度是：{train_X.shape}, 测试集的尺度是：{test_X.shape}")
 # ----------------------------->第一题（必做）
 # Initialize the Logistic Regression model
-logistic_regression = LogisticRegression()
+logistic_regression = LogisticRegression(solver='sag')
 
 # Fit the model on the training data and labels
 logistic_regression.fit(train_X_flat, train_size)
@@ -82,41 +82,35 @@ print("aaaaaaaaaaaaaaaaa")
 
 # Make predictions on the test set
 test_number_predictions = softmax_regression.predict(test_X_flat)
+test_number_predictions_proba = softmax_regression.predict_proba(test_X_flat)
+print(test_number_predictions)
 
 # 计算准确率
 accuracy = accuracy_score(test_number, test_number_predictions)
 
 # 计算精确率
-precision = precision_score(test_number, test_number_predictions, average='micro')
+precision = precision_score(test_number, test_number_predictions, average='macro')
 
 # 计算召回率
-recall = recall_score(test_number, test_number_predictions)
+recall = recall_score(test_number, test_number_predictions, average='macro')
 
 # 计算F1-score
-f1 = f1_score(test_number, test_number_predictions)
+f1 = f1_score(test_number, test_number_predictions, average='macro')
+print("准确率 (Accuracy): {:.2f}".format(accuracy))
+print("精确率 (Precision): {:.2f}".format(precision))
+print("召回率 (Recall): {:.2f}".format(recall))
+print("F1-score: {:.2f}".format(f1))
 
 # 计算auROC
-au_roc = roc_auc_score(test_number, test_number_predictions)
+au_roc = roc_auc_score(test_number, test_number_predictions_proba, multi_class='ovr')
 
-# 计算 ROC 曲线
-fpr, tpr, thresholds = roc_curve(test_number, test_number_predictions)
-
-# 绘制 ROC 曲线
-plt.figure()
-plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % au_roc)
-plt.plot([0, 1], [0, 1], 'k--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('2-Receiver Operating Characteristic (ROC)')
-plt.legend(loc="lower right")
-plt.show()
+cm = confusion_matrix(test_number, test_number_predictions)
 
 print("准确率 (Accuracy): {:.2f}".format(accuracy))
 print("精确率 (Precision): {:.2f}".format(precision))
 print("召回率 (Recall): {:.2f}".format(recall))
 print("F1-score: {:.2f}".format(f1))
 print("auROC: {:.2f}".format(au_roc))
+print("confusion_matrix: ", cm)
 
 
